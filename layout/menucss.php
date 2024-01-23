974790c2983047bd84804081d075c995081f0683
HP196\CES <informatica@escuelaces.com>
2024-01-23 11:23:23 +0100

    commit

---
+++ layout/menucss.php
@@ -0,0 +1,67 @@
+<div class="topnav" id="myTopnav">
+        
+<a href="../../index.html" class="active">Inicio</a>
+        
+
+
+<a href="../bootstrap/bootstrap.php" class="dropdown">Bootstrap</a>
+
+<a href="../parallax/parallax.php" class="dropdown">Parallax</a>
+
+
+
+
+
+
+
+        <div class="dropdown">
+            <button class="dropbtn">Rwd & Awd 
+              <i class="fa fa-caret-down"></i>
+            </button>
+            <div class="dropdown-content">
+              <a href="layout/camisetas.php">Camisetas</a>
+              <a href="../../css/RWD/Detalle-Pelicula.php">Película</a>
+              <a href="https://solired.es/openaula/Promociones/Promocion-2023-GDAM-1-1/" target="_blank">Orla</a>
+              <a href="#">Apuestas</a>
+              
+            </div>
+          </div> 
+        
+        <div class="dropdown">
+            <button class="dropbtn">FlexBox 
+              <i class="fa fa-caret-down"></i>
+            </button>
+            <div class="dropdown-content">
+              <a href="../../css/flexbox/ACT-1-coches-peugeot.php">Coches</a>
+              <a href="../../css/flexbox/protonvpn.php">Proton VPN</a>
+              <a href="../../css/flexbox/operadora-flex.php">Operadora</a>
+              
+            </div>
+          </div> 
+
+        <div class="dropdown">
+          <button class="dropbtn">BoxModel 
+            <i class="fa fa-caret-down"></i>
+          </button>
+          <div class="dropdown-content">
+            <a href="../../css/BoxModel/modeloX.php">Modelo X</a>
+            <a href="../../css/BoxModel/modeloY.php">Modelo Y</a>
+            <a href="../../css/BoxModel/portada-A.php">Deportes</a>
+            <a href="../../css/BoxModel/operadora.php">Operadora (Lidertel)</a>
+            <a href="#">Optativa 2</a>
+          </div>
+        </div> 
+        <a href="javascript:void(0);" style="font-size:15px;" class="icon" onclick="myFunction()">&#9776;</a>
+      </div>
+      
+           
+      <script>
+      function myFunction() {
+        var x = document.getElementById("myTopnav");
+        if (x.className === "topnav") {
+          x.className += " responsive";
+        } else {
+          x.className = "topnav";
+        }
+      }
+      </script>

8c1d27c92d064937ca6648dba1d252f3786d9ad3
HP196\CES <informatica@escuelaces.com>
2024-03-04 12:14:11 +0100

    js

--- LM1GDAM/layout/rwd/camisetas.php
+++ LM1GDAM/layout/rwd/camisetas.php
@@ -6,7 +6,8 @@
     <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
     <link rel="icon" href="favicon.ico" type="image/x-icon">
     
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
     <title>La Tostadora</title>
     
     <meta name="title" content="La Tostadora">
